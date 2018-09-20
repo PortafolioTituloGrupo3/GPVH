4eba52c6d8cf1918ed029cbeb66225f0fb31bf54
Christian Matte <ch.matte@alumnos.duoc.cl>
2018-09-20 18:54:32 -0300

    Se agrego la capa de negocio y clases funcionario, usuario y unidad, y ademas, un enum TipoUsuario.

---
+++ LB_GPVH/Clases/Unidad.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LB_GPVH.Clases
+{
+    public class Unidad
+    {
+        private int id;
+        private string nombre;
+        private string descripcion;
+        private string direccion;
+        private bool habilitado;
+        private int idPadre;
+        private int runJefe;
+
+        public Unidad()
+        {
+
+        }
+
+        public int RunJefe
+        {
+            get { return runJefe; }
+            set { runJefe = value; }
+        }
+
+        public int IdPadre
+        {
+            get { return idPadre; }
+            set { idPadre = value; }
+        }
+
+
+        public bool Habilitado
+        {
+            get { return habilitado; }
+            set { habilitado = value; }
+        }
+
+
+        public string Direccion
+        {
+            get { return direccion; }
+            set { direccion = value; }
+        }
+
+
+        public string Descripcion
+        {
+            get { return descripcion; }
+            set { descripcion = value; }
+        }
+
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+    }
+}
---
+++ WF_GPVH/Formularios/Login/Form_Login.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace WF_GPVH.Formularios.Login
+{
+    partial class Form_Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblClave = new System.Windows.Forms.Label();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(47, 199);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(147, 20);
+            this.lblNombre.TabIndex = 0;
+            this.lblNombre.Text = "Nombre de usuario:";
+            // 
+            // lblClave
+            // 
+            this.lblClave.AutoSize = true;
+            this.lblClave.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblClave.ForeColor = System.Drawing.Color.White;
+            this.lblClave.Location = new System.Drawing.Point(47, 227);
+            this.lblClave.Name = "lblClave";
+            this.lblClave.Size = new System.Drawing.Size(130, 20);
+            this.lblClave.TabIndex = 1;
+            this.lblClave.Text = "Clave de usuario:";
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.BackColor = System.Drawing.Color.MediumBlue;
+            this.btnIniciar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIniciar.ForeColor = System.Drawing.Color.White;
+            this.btnIniciar.Location = new System.Drawing.Point(291, 335);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(120, 45);
+            this.btnIniciar.TabIndex = 2;
+            this.btnIniciar.Text = "Iniciar Sesión";
+            this.btnIniciar.UseVisualStyleBackColor = false;
+            // 
+            // Form_Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(447, 414);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.lblClave);
+            this.Controls.Add(this.lblNombre);
+            this.Name = "Form_Login";
+            this.Text = "Form_Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblClave;
+        private System.Windows.Forms.Button btnIniciar;
+    }
+}
