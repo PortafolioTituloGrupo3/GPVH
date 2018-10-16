0c7b128b78173c4c4cd65c7b2ddb234bc0a88c38
Christian Matte <ch.matte@alumnos.duoc.cl>
2018-10-15 21:49:07 -0300

    Se agrego resolucion como clase

---
+++ LB_GPVH/Modelo/Resolucion.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LB_GPVH.Enums;
+
+namespace LB_GPVH.Modelo
+{
+    public class Resolucion
+    {
+        private int id;
+        private DateTime fechaResolucion;
+        public EstadoResolucion Estado { get; set; }
+        private Permiso permiso;
+        private Funcionario resolvente;
+
+        public Resolucion()
+        {
+            id = -1;
+            permiso = null;
+            resolvente = null;
+        }
+
+        public Funcionario Resolvente
+        {
+            get { return resolvente; }
+            set { resolvente = value; }
+        }
+
+
+        public Permiso Permiso
+        {
+            get { return permiso; }
+            set { permiso = value; }
+        }
+
+
+
+        public DateTime FechaResolucion
+        {
+            get { return fechaResolucion; }
+            set { fechaResolucion = value; }
+        }
+
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+    }
+}
